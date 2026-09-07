19859d765b941133cc23fcca0d2311540ff56838
CgAtieqah <cikguatieqah@gmail.com>
2026-09-07 21:52:13 +0800

    tambah sa

--- lessons.js
+++ lessons.js
@@ -6,10 +6,17 @@ const lessons = [
     highlight1: 200
    
   },
-{
+  {
     id: 'ru',
     part1: 'ru',
     audio: 'audio/ru.m4a',
+    highlight1: 200
+   
+  },
+  {
+    id: 'sa',
+    part1: 'sa',
+    audio: 'audio/sa.m4a',
     highlight1: 200
    
   },
